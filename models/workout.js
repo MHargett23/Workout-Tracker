85816e0cfd8c10f63bf6958bae0d1485ddeadffb
Martin <melloncollie221@aol.com>
2020-03-31 10:05:53 -0400

    reorganized files

---
+++ models/workout.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema (
+    {
+        day: {
+            type: Date,
+            default: ()=> new Date()
+        },
+        excercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: " Please enter excercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an excercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter excercise length"
+                },
+                weight: {
+                    type: Number,
+
+                },
+                reps: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                distance: {
+                    type: Number,
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+          virtuals: true,  
+        }
+    }
+);
+
+workoutSchema.virtual("totalDuration").get(function (){
+    return this.excercises.reduce((total, exercise)=>{
+        return total + excercise.duration
+    }, 0)
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
